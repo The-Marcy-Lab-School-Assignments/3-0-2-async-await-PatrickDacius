e923314de9519180d0d93a9ef60db8a05ee40cd6
Patrick Dacius <Patrickdacius27@gmail.comgit config --global credential.helper store>
2024-06-16 18:03:21 -0400

    finished

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,2 +1,28 @@
-export const fetchHandler = () => {
+export const fetchHandler = async (url, options = {}) => {
+    try {
+        const newFetchedData = await fetch(url, options);
+
+        if (!newFetchedData.ok) {
+            throw new Error(`Fetch failed with status - ${newFetchedData.status}, ${newFetchedData.text}`);
+        }
+
+
+        const contentType = newFetchedData.headers.get('content-type');
+        const isJson = contentType && contentType.includes('application/json');
+
+        let data = undefined;
+        if (isJson) {
+            data = await newFetchedData.json();
+        } else {
+            data = await newFetchedData.text();
+        }
+
+        const tuple = [data, null];
+        return tuple;
+    } catch (error) {
+        console.warn(error); // Log the error
+        const tuple = [null, error];
+        return tuple;
+    }
 };
+
